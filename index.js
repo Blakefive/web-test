54969d8de0bddd17706740d03fe9536214632874
Blakefive <fivefive050500@gmail.com>
2022-02-01 14:33:46 +0000

    gg

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express');
-var mysql = require('mysql');
-const app = express();
-const port = process.env.PORT || 3000;
-
-(async function(){
-    let db = mysql.createPool({
-        host: "us-cdbr-east-05.cleardb.net",
-        user: "b80bb3614e25cd",
-        password: "30a5c7bf",
-        database: "heroku_96a107df1e5e545"
-      });
-
-let insertSql = 'INSERT INTO bmokey(name) VALUES("W")';
-let selectSql = 'SELECT *FROM bmokey';
-
-let connection = await db.getConnection(async conn => conn);
-//await connection.beginTransaction();
-let bmokey = await connection.query(insertSql);
-await connection.commit();
-await connection.release();
-
-let get_connection = await db.getConnection(async conn => conn);
-let [user] = await get_connection.query(selectSql);
-
-app.get('/', async(req, res) => {res.send(user); get_connection.release();})
-app.listen(port, () => {console.log("Example app listening on port ${port}");});
-    })()
+const mysql = require('mysql');
+/* Step 1, create DB Pool */
+const pool = mysql.createPool({
+	host: 'us-cdbr-east-05.cleardb.net',
+	user: '"b80bb3614e25cd",',
+	password: '30a5c7bf',
+	database: 'heroku_96a107df1e5e545'
+});
+/* Step 2. get connection */
+const dbTest = async () => {
+	try {
+		const connection = await pool.getConnection(async conn => conn);
+		try {
+			/* Step 3. */
+			const name = 'HELLO';
+			await connection.beginTransaction(); // START TRANSACTION
+			const [rows] = await connection.query('INSERT INTO bmokey(name) VALUES(?)', [name]);
+			await connection.commit(); // COMMIT
+			connection.release();
+			return rows;
+		} catch(err) {
+			await connection.rollback(); // ROLLBACK
+			connection.release();
+			console.log('Query Error');
+			return false;
+		}
+	} catch(err) {
+		console.log('DB Error');
+		return false;
+	}
+};
