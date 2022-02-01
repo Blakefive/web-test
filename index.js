a8803b549aaa515ebfc227be24df6d29e3f27a0d
Blakefive <fivefive050500@gmail.com>
2022-02-01 11:58:31 +0000

    gg

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var {config} = require('./config/db.config')
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(port))
+app.get('/', (req, res) => res.send(config))
 app.listen(port, () => {console.log("Example app listening on port ${port}");});
 
