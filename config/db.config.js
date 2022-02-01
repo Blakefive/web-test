3fb2f54d910c06fabd982f902788a351aff836b2
Blakefive <fivefive050500@gmail.com>
2022-02-01 11:45:31 +0000

    gg

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+/*module.exports = {
     HOST: "us-cdbr-east-05.cleardb.net",
     USER: "b80bb3614e25cd",
     PASSWORD: "30a5c7bf",
     DB: "heroku_96a107df1e5e545"
   };
-  const check = "Hello World";
+  */
+ const check = "Hello World!! (check)";
+ module.exports = {check};
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const config = require('./config/db.config.js')
+const {config} = require('./config/db.config.js')
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(config.check))
+app.get('/', (req, res) => res.send(config))
 app.listen(port, () => {console.log("Example app listening on port ${port}");});
 
