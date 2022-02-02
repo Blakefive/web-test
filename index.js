70b8d3270f99bf303345d45198b1e9655cf9045f
Blakefive <fivefive050500@gmail.com>
2022-02-02 12:34:57 +0000

    gg

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-const mysql = require('mysql');
 const express = require('express');
+var edit_data = require('./file/edit_data.js');
+
 const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
 const port = process.env.PORT || 3000;
 
-/* Step 1, create DB Pool */
-const pool = mysql.createPool({
-	host: 'us-cdbr-east-05.cleardb.net',
-	user: '"b80bb3614e25cd",',
-	password: '30a5c7bf',
-	database: 'heroku_96a107df1e5e545'
+app.get('/', async(req, res) => {
+    res.render(__dirname + '/index.html');
 });
-/* Step 2. get connection */
-const dbTest = async () => {
-		const connection = await pool.getConnection(async conn => conn);
-		/* Step 3. */
-		const name = 'HELLO';
-		await connection.beginTransaction(); // START TRANSACTION
-		const [rows] = await connection.query('INSERT INTO bmokey(name) VALUES(?)', [name]);
-		await connection.commit(); // COMMIT
-		connection.release();
-		return rows;
-};
-
-app.get('/', (req, res) => {res.send("Insert Data");})
-app.listen(port, () => {console.log("Example app listening on port ${port}");})
+app.post('/', (req, res) => {
+    console.log(req.body.key);
+    edit_data(req.body.key);
+    //res.render(__dirname + '/index.html', {'data' : req.body})
+    res.redirect('/');
+});
+app.listen(port, () => {
+    console.log("Server Start");
+});
